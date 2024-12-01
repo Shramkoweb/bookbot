3d9722e6ed8aa2c2889e9f07e1b640fdd94bb23d
Serhii Shramko <shramko.web@yahoo.com>
2024-11-30 22:50:25 -0500

    refactor: wrap with main

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+
+if __name__ == "__main__":
+    main()
