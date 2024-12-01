125175ffdb742ddef04d412308e9a50c4547ffa6
Serhii Shramko <shramko.web@yahoo.com>
2024-11-30 22:59:55 -0500

    feat: add count_chars

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def main():
     text = get_book_text("books/frankenstein.txt")
     words_amount = count_words(text)
-    print(f"{words_amount} words found in the document")
+    chars_dict = count_chars(text)
+    print(chars_dict)
 
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_chars(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
