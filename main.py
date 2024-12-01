5a227d4f819960f43143c3e38d584dc10a5e43ce
Serhii Shramko <shramko.web@yahoo.com>
2024-11-30 23:26:08 -0500

    feat: add report

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
     words_amount = count_words(text)
     chars_dict = count_chars(text)
-    print(chars_dict)
+    result = chars_dict_to_sorted(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words_amount} words found in the document")
+    print()
+
+    for item in result:
+        if not item['char'].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def chars_dict_to_sorted(chars_dict):
+    result = []
+    for char, value in chars_dict.items():
+        result.append({"char": char, "num": value})
+    result.sort(reverse=True, key=sort_chars)
+    return result
+
+
+def sort_chars(d):
+    return d['num']
+
 
 def count_words(text):
     words = text.split()
