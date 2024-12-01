8fcf77326831d201eb3baa2439266f6a6e74f6c6
Serhii Shramko <shramko.web@yahoo.com>
2024-11-30 22:56:30 -0500

    feat: add count_words

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    text = get_book_text("books/frankenstein.txt")
+    words_amount = count_words(text)
+    print(f"{words_amount} words found in the document")
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 if __name__ == "__main__":
