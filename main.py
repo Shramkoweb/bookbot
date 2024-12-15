90aef95570bd0d5fa3a05e603a427629b417ed5b
Serhii Shramko <shramko.web@yahoo.com>
2024-12-14 23:36:48 -0500

    refactor: add types & improve readability

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from typing import List, Dict
+from collections import Counter
+
+REPORT_HEADER = "--- Begin report of {} ---"
+REPORT_FOOTER = "--- End report ---"
+WORD_COUNT_MESSAGE = "{} words found in the document\n"
+CHAR_COUNT_MESSAGE = "The '{}' character was found {} times"
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -5,18 +13,18 @@ def main():
     chars_dict = count_chars(text)
     result = chars_dict_to_sorted(chars_dict)
 
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{words_amount} words found in the document\n")
+    print(REPORT_HEADER.format(book_path))
+    print(WORD_COUNT_MESSAGE.format(words_amount))
 
     for item in result:
         if not item['char'].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(CHAR_COUNT_MESSAGE.format(item['char'], item['num']))
 
-    print("--- End report ---")
+    print(REPORT_FOOTER)
 
 
-def chars_dict_to_sorted(chars_dict):
+def chars_dict_to_sorted(chars_dict: Dict[str, int]) -> List[Dict[str, int]]:
     result = []
     for char, value in chars_dict.items():
         result.append({"char": char, "num": value})
@@ -24,22 +32,19 @@ def chars_dict_to_sorted(chars_dict):
     return result
 
 
-def sort_chars(d):
+def sort_chars(d: Dict[str, int]) -> int:
     return d['num']
 
 
-def count_words(text):
+def count_words(text: str) -> int:
     return len(text.split())
 
 
-def count_chars(text):
-    chars = {}
-    for c in text.lower():
-        chars[c] = chars.get(c, 0) + 1
-    return chars
+def count_chars(text: str) -> Dict[str, int]:
+    return Counter(text.lower())
 
 
-def get_book_text(path):
+def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
 
